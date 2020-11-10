c1eef15a35722dddc58f2dad41cb8f200990d8d5
Christian Carnalla <ccarnalla4514@stu.d214.org>
2020-11-10 15:26:38 -0600

    added IB Outlets and multiplying code but handshake error needs to be fixed with the unbutton.

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstValue: UITextField!
+    
+    @IBOutlet weak var secondValue: UITextField!
+    
+    @IBOutlet weak var product: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        
+        let variableA = Double(firstValue.text!)
+        let variableB = Double(secondValue.text!)
+        let answer = (variableA! * variableB!)
+        
+        product.text = "\(answer)"
+    }
+
+
 
 }
-
